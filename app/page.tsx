bf6b2cadf6e97e631c841e00b4c18704341c15b7
nur-nayeem <nurnayem768@gmail.com>
2025-10-27 17:08:54 +0600

    designed new snippet create page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="my-container mx-auto h-screen pt-6">
@@ -5,9 +7,11 @@ export default function Home() {
       <div className="mt-4">
         <div className="flex justify-between items-center mb-6">
           <h3 className="text-2xl">Snippets</h3>
-          <button className="bg-green-600 text-white rounded-lg p-2.5">
-            New
-          </button>
+          <Link href={"/snippet/new"}>
+            <button className="bg-green-600 text-white rounded-lg p-2.5">
+              New
+            </button>
+          </Link>
         </div>
         <div className="space-y-2.5">
           <div className="flex justify-between items-center bg-gray-100 py-2.5  px-3.5 rounded-md">
--- app/snippet/new/page.tsx
+++ app/snippet/new/page.tsx
@@ -1,7 +1,43 @@
+"use client";
+import { Editor } from "@monaco-editor/react";
 import React from "react";
 
 const NewSnippet = () => {
-  return <div></div>;
+  return (
+    <div className="my-container pt-5">
+      <h2 className="text-3xl my-3.5">Create New Snippet</h2>
+      <div className="w-full">
+        <form action="" className="w-full space-y-3.5">
+          <div className="flex flex-col gap-1.5">
+            <label className="text-lg font-medium">Title:</label>
+            <input
+              className="border border-gray-300 p-2.5 rounded-lg focus:outline-none"
+              type="text"
+              name="title"
+              placeholder="title of the snippet"
+            />
+          </div>
+          <div className="space-y-1.5">
+            <label className="text-lg font-medium">Code:</label>
+            <Editor
+              height="50vh"
+              defaultLanguage="javascript"
+              defaultValue={`// // some comments
+// const add = ()=>{
+//     return a + b;
+// }`}
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-black/80 text-white py-2.5 rounded-lg"
+          >
+            Save
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default NewSnippet;
